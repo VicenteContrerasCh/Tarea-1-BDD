192c4b6b80405397ed063888661d0a3656e99fc3
Benjamin Ulloa <benjaulloa0@gmail.com>
2026-04-04 17:48:09 -0300

    base funcional

---
+++ baseDatos.py
@@ -0,0 +1,23 @@
+#Importa la librería psycopg2 para conectarse a la base de datos PostgreSQL
+import psycopg2
+#Intenta establecer una conexión con la base de datos utilizando las credenciales proporcionadas
+try:
+    conn = psycopg2.connect(
+        dbname="tareaBDD1",
+        user="postgres",
+        password="Zpmgjdh_12",
+        host="localhost",
+        port="5432"
+    )
+except:
+    print("Error al conectar a la base de datos")
+#Intenta ejecutar una consulta SQL para seleccionar todos los registros de la tabla "Jugadores"
+query = "SELECT * FROM Jugadores;"
+curr = conn.cursor()
+try:
+    curr.execute(query)
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+except psycopg2.Error as e:
+    print("Error al ejecutar la consulta:", e.pgcode)
